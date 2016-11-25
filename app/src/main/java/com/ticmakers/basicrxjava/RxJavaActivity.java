99140b4087645b3f9d353bd6f55bf0432f024f7c
santiago17 <santiago.oroz17@gmail.com>
2016-11-25 13:45:18 -0500

    Ejemplo de operaciones con RxJava

---
+++ app/src/main/java/com/ticmakers/basicrxjava/RxJavaActivity.java
@@ -0,0 +1,15 @@
+package com.ticmakers.basicrxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RxJavaActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_java);
+    }
+}
